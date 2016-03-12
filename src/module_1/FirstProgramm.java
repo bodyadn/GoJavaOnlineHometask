fe51fe9eeb1d8f259addecf2aa35611a86907ffb
Bogdan <bogdan.gutorovych@gmail.com>
2016-03-12 10:28:40 +0200

    Add first module

---
+++ src/module_1/FirstProgramm.java
@@ -0,0 +1,7 @@
+package module_1;
+
+public class FirstProgramm {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
